fb0b75abb00bffc2b9ac1512c471f1745c248a3a
nicolás_carratú <carratutommasino@gmail.com>
2022-08-26 18:25:20 -0300

    .

--- js/products.js
+++ js/products.js
@@ -1,9 +1,10 @@
 let currentProductsArray = [];
 const ORDER_BY_SOLD_COUNT = "Cant.";
-const ORDER_BY_PRICE  = 'Price';
+const ORDER_BY_PRICE = 'Price';
 let currentSortCriteria = undefined;
 let minCount = undefined;
 let maxCount = undefined;
+let search = document.getElementById('search');
 function sortProducts(criteria, array) {
 
     let result = [];
@@ -38,32 +39,34 @@ function sortProducts(criteria, array) {
 
 function showProductsList(array) {
     let htmlContentToAppend = "";
-    console.log(array)
     for (let i = 0; i < array.length; i++) {
         let product = array[i];
-
+        let search_val = search.value;
         if (((minCount == undefined) || (minCount != undefined && parseInt(product.cost) >= minCount)) &&
             ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))) {
 
-            htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action cursor-active">
-            <div class="row">
-                <div class="col-3">
-                    <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">${product.name}</h4>
-                        <h4 class="mb-1">${product.currency} ${product.cost}</h4>
-                        <small class="text-muted">${product.soldCount} artículos</small>
+            if (product.name.toLowerCase().includes(search_val.toLowerCase()) || product.description.toLowerCase().includes(search_val.toLowerCase())) {
+                htmlContentToAppend += `
+                <div class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">${product.name}</h4>
+                                <h4 class="mb-1">${product.currency} ${product.cost}</h4>
+                                <small class="text-muted">${product.soldCount} artículos</small>
+                            </div>
+                            <p class="mb-1">${product.description}</p>
+                        </div>
                     </div>
-                    <p class="mb-1">${product.description}</p>
                 </div>
-            </div>
-        </div>
-        `
+                `
+            }
+
         }
-        document.getElementById("cars").innerHTML = htmlContentToAppend;
+        document.getElementById("products").innerHTML = htmlContentToAppend;
     }
 }
 function sortAndShowProducts(sortCriteria, productsArray) {
@@ -82,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(url).then(function (resultObj) {
         if (resultObj.status === "ok") {
             currentProductsArray = resultObj.data.products;
-            
+
             showProductsList(currentProductsArray);
 
         }
@@ -100,30 +103,31 @@ document.addEventListener("DOMContentLoaded", function (e) {
     document.getElementById("sortByProdCount").addEventListener("click", function () {
         sortAndShowProducts(ORDER_BY_SOLD_COUNT);
     });
+    document.getElementById("filtroRange").addEventListener("click", function () {
 
+        minCount = document.getElementById("filtroMin").value;
+        maxCount = document.getElementById("filtroMax").value;
+
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0) {
+            minCount = parseInt(minCount);
+        }
+        else {
+            minCount = undefined;
+        }
+
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0) {
+            maxCount = parseInt(maxCount);
+        }
+        else {
+            maxCount = undefined;
+        }
+        showProductsList(currentProductsArray);
+    });
+
+    search.addEventListener('input', function () {
+        showProductsList(currentProductsArray)
+    })
 })
-
-
-document.getElementById("filtroRange").addEventListener("click", function () {
-
-    minCount = document.getElementById("filtroMin").value;
-    maxCount = document.getElementById("filtroMax").value;
-
-    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0) {
-        minCount = parseInt(minCount);
-    }
-    else {
-        minCount = undefined;
-    }
-
-    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0) {
-        maxCount = parseInt(maxCount);
-    }
-    else {
-        maxCount = undefined;
-    }
-    showProductsList(currentProductsArray);
-});
 
 
 
