c3df2b052e337e5017024ae8ef2e330e8059444f
nicolás_carratú <carratutommasino@gmail.com>
2022-09-13 18:57:51 -0300

    .

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,8 @@
 let prodID = sessionStorage.getItem('prodID')
 let url = PRODUCT_INFO_URL + prodID + '.json'
 let comments = PRODUCT_INFO_COMMENTS_URL + prodID + '.json'
+let mostrarTodos = ''
+let boton_comments = document.getElementById("showAllOrLess")
 
 function showProduct(array) {
     let product = array;
@@ -51,48 +53,89 @@ function showProduct(array) {
 }
 
 function showComments(array) {
-    //let estrellas = []
     let stars = ''
     let comentarios_list = array
     let comentarios = ""
+    if (array.length > 1) {
+        mostrarTodos = `
+    <button class="btn btn-light btn-lg btn-block">Mostrar todos los comentarios</button>
+    `
+        boton_comments.innerHTML = mostrarTodos;
+    }
     for (let i = 0; i < array.length; i++) {
+        comentarios=''
+        stars=''
         comentarios_list = array[i];
         comentarios += `
-        <div>
+        <div class='comment ${i > 1 ? 'hide' : ''}'>
         <div class='stars'> </div>
-        <p> ${comentarios_list.description}</p>
+          <p> ${comentarios_list.description}</p>
         <p> ${comentarios_list.user}</p>
         <p> ${comentarios_list.dateTime}</p>
+        <hr>
+        </div> 
         </div>
         `
-    }
-    document.getElementById("comments").innerHTML += comentarios;
+        document.getElementById("contenedor").innerHTML += comentarios;
 
-    for (let i = 0; i < array.length; i++) {
-        stars = '';
-        comentarios_list = array[i]
-        /* stars = `
-         <p> ${comentarios_list.score} </p>
-         <span class="fa fa-star"></span>
-         <span class="fa fa-star"></span>
-         <span class="fa fa-star"></span>
-         <span class="fa fa-star"></span>
-         <span class="fa fa-star"></span>
-        ` */
+        for (let i = 0; i < 5; i++) {
 
-        for (let i = 0;i < 5; i++){
             stars += `<span class="fa fa-star ${comentarios_list.score > i ? 'checked' : ''}"></span>`
-
+            
         }
         document.getElementsByClassName("stars")[i].innerHTML = stars;
-        //estrellas.push(document.getElementsByClassName("stars")[i])
-
-        /* for (let i = 0; i < comentarios_list.score; i++) {
-            document.getElementsByClassName("fa-star")[i].classList.add('checked')
-            } */
-        } 
-        
     }
+
+}
+
+function newOne() {
+    let stars = ''
+    let comentario_nuevo = document.getElementById('comment-box').value
+    let comentario_nuevo_final = ''
+    let estrellas_nuevo = document.getElementById('cantidad').value
+    comentario_nuevo_final += 
+    `<div class='comment'>
+        <div class='stars'> </div>
+        <p> ${comentario_nuevo} </p>
+    <hr>`
+    
+    for (let i = 0; i < 5; i++) {
+            stars += `<span class="fa fa-star ${estrellas_nuevo > i ? 'checked' : ''}"></span>`
+    }
+    document.getElementById('contenedor').innerHTML = comentario_nuevo_final
+   
+    showComments(comments_list)
+    document.getElementsByClassName("stars")[0].innerHTML = stars
+}
+
+function showAllOrLess() {
+    if (document.getElementsByClassName('all')[0]) {
+        for (let i = 0; i < document.getElementsByClassName('hide').length; i++) {
+            document.getElementsByClassName('hide')[i].classList.add('show')
+        }
+        mostrarTodos = `
+    <button class="btn btn-light btn-lg btn-block">Mostrar menos comentarios</button>
+    `
+        boton_comments.innerHTML = mostrarTodos;
+        boton_comments.classList.remove('all')
+        boton_comments.classList.add('less')
+
+    } else if(document.getElementsByClassName('less')[0]) {
+
+
+        for (let i = 0; i < document.getElementsByClassName('hide').length; i++) {
+            document.getElementsByClassName('hide')[i].classList.remove('show')
+        }
+        mostrarTodos = `
+    <button class="btn btn-light btn-lg btn-block">Mostrar todos los comentarios</button>
+    `
+        boton_comments.innerHTML = mostrarTodos;
+        boton_comments.classList.remove('less')
+        boton_comments.classList.add('all')
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(url).then(function (resultObj) {
         if (resultObj.status === "ok") {
@@ -107,7 +150,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
             })
         }
     })
+
+    document.getElementById('showAllOrLess').addEventListener('click', function () {
+        showAllOrLess()
+    })
+
+    document.getElementById('envio').addEventListener('click', function () {
+        newOne()
+    })
 })
+
+
 
 
 
