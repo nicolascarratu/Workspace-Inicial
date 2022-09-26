dcf8925e07dcc9d17ca3f72d165db618b8d1fb41
nicolás_carratú <carratutommasino@gmail.com>
2022-09-26 16:47:20 -0300

    .

--- js/user.js
+++ js/user.js
@@ -2,6 +2,11 @@
 var user = localStorage.getItem('User')
 document.getElementsByClassName('user')[0].innerHTML = user
 
+function deleteUser() {
+    localStorage.removeItem('User')
+}
+
+
 //La declaro en este js para asi se puede usar tambien en la pagina principal, y no solo en la de categorias.
 function setCatID(id) {
     sessionStorage.setItem("catID", id);
