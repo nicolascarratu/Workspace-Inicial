52d80b9f92629a6a28a06754ce70d3b697f278ea
nicolás_carratú <carratutommasino@gmail.com>
2022-10-18 14:00:58 -0300

    .

--- js/cart.js
+++ js/cart.js
@@ -5,7 +5,6 @@ let cartObject = []
 
 function mostrarCarrito(object) {
     let HTMLtext = ''
-    console.log(object)
     for (let i = 0; i < object.length; i++) {
         let cart = object[i]
         HTMLtext += `
@@ -21,7 +20,7 @@ function mostrarCarrito(object) {
                     <a onclick="setProdID(${cart.id})"> ${cart.name}</a>
                 </div>
                 <div class="col-2">
-                    <p> ${cart.currency} ${cart.unitCost}</p>
+                    <p> USD ${cart.unitCost}</p>
                 </div>
                 <div class="col-2">
                     <input type="number" class='w-50' id="cant_unit${i}" oninput='result(cartArticlesList)' value='1' min='1'>
@@ -45,31 +44,58 @@ function mostrarCarrito(object) {
 
 function result(array) {
     let cart = array
-    let resultParcial = 0
-    let resultTotal = 0
+    let resultSubtotalInd = 0
+    let resultEnvio = 0
+    let resultSubTotalGeneral = 0
+    let precioEnDolares = 0
+    let resutlFinal = 0
+
     for (let i = 0; i < cart.length; i++) {
-        resultParcial = cart[i].unitCost * document.getElementById(`cant_unit${i}`).value
-        document.getElementById(`subtotal${i}`).innerHTML = cart[i].currency + ' ' + resultParcial
-        resultTotal += resultParcial
-        document.getElementById('total').innerHTML = 'Total: ' + cart[i].currency + ' ' + resultTotal
+        if (cart[i].currency == 'UYU') {
+            precioEnDolares = Math.round(cart[i].unitCost / 41)
+        }
+        else {
+            precioEnDolares = cart[i].unitCost
+        }
+        resultSubtotalInd = precioEnDolares * document.getElementById(`cant_unit${i}`).value
+        document.getElementById(`subtotal${i}`).innerHTML = 'USD ' + resultSubtotalInd
+        resultSubTotalGeneral += resultSubtotalInd
+        document.getElementById('subtotal').innerHTML = 'USD ' + resultSubTotalGeneral
+    }
+    if (document.getElementById('premium').checked) {
+        resultEnvio = resultSubTotalGeneral * 0.15
+    }
+    else if (document.getElementById('express').checked) {
+        resultEnvio = resultSubTotalGeneral * 0.07
+    }
+    else {
+        resultEnvio = resultSubTotalGeneral * 0.05
     }
 
+    resutlFinal = resultSubTotalGeneral + resultEnvio
+
+    document.getElementById('porcentaje').innerHTML = 'USD ' + Math.round(resultEnvio)
+    document.getElementById('total').innerHTML = 'USD ' + Math.round(resutlFinal)
+
+    
+
+}
 
 // Una vez cargada la página, obtengo el JSON con el producto para mostrar en el carrito. Llamo a la función
 // que se encarga de mostrarlo, y también a la calcula el subtotal. Por último, si se modifica la cantidad de 
 // unidades a comprar, se vuelve a llamar a la función subtotal() para actualizar el precio a pagar. 
 
 document.addEventListener('DOMContentLoaded', function () {
-    getJSONData(CART_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            cartArray = resultObj.data.articles[0]
-            cartArticlesList = JSON.parse(localStorage.getItem('cart'))
-            mostrarCarrito(cartArticlesList)
-            result(cartArticlesList)
-            console.log(cartArticlesList)
+    /*  getJSONData(CART_URL).then(function (resultObj) {
+         if (resultObj.status === "ok") {
+             cartObject = resultObj.data.articles[0] */
+    cartArticlesList = JSON.parse(localStorage.getItem('cart'))
+    mostrarCarrito(cartArticlesList)
+    result(cartArticlesList)
 
 }
+)
+/*  }
 
-    }
-
-)})
+)}
+)} */
